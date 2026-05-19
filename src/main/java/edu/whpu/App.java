cc4038a32ae0c4dc5f4a3542a3feea2987e8b8e1
文斌01 <758897@ky-tech.com.cn>
2026-05-19 19:16:25 +0800

    init

--- src/main/java/edu/whpu/App.java
+++ src/main/java/edu/whpu/App.java
@@ -2,6 +2,7 @@ package edu.whpu;
 
 import com.jd.platform.hotkey.client.ClientStarter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,14 +19,27 @@ public class App {
         SpringApplication.run(App.class,args);
     }
 
+    @Value("${hotkey.app-name}")
+    private String appName;
+
+    @Value("${hotkey.etcd-server}")
+    private String etcdServer;
+
+    @Value("${hotkey.caffeine-size}")
+    private int caffeineSize;
+
+    @Value("${hotkey.push-period}")
+    private Long pushPeriod;
+
     @PostConstruct
     public void initCache(){
         ClientStarter.Builder builder = new ClientStarter.Builder();
-        ClientStarter starter = builder.setAppName("")
-                .setEtcdServer("")
-                .setCaffeineSize(12)
-                .setPushPeriod(10000L)
+        ClientStarter starter = builder.setAppName(appName)
+                .setEtcdServer(etcdServer)
+                .setCaffeineSize(caffeineSize)
+                .setPushPeriod(pushPeriod)
                 .build();
         starter.startPipeline();
     }
+
 }
